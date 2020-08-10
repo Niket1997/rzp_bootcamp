d0b532c1aef8c3c0d5603b6486c26bca3b0fbe86
Niket1997 <aniket.mahangare1997@gmail.com>
2020-08-10 11:24:15 +0530

    added json support

--- urlshortener/handler.go
+++ urlshortener/handler.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 
 	"gopkg.in/yaml.v2"
 )
 
+// MapHandler to handler map inputs
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path
@@ -19,20 +20,37 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 	}
 }
 
+// YAMLHandler to handle yaml file
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
 	var urlsPath []urlPath
 	err := yaml.Unmarshal(yml, &urlsPath)
-	fmt.Println(urlsPath)
 	if err != nil {
 		return nil, err
 	}
+	return MapHandler(createMap(urlsPath), fallback), nil
+}
+
+// JSONHandler to handle JSON file
+func JSONHandler(jsn []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	var urlsPath []urlPath
+	err := json.Unmarshal(jsn, &urlsPath)
+	if err != nil {
+		return nil, err
+	}
+
+	return MapHandler(createMap(urlsPath), fallback), nil
+}
+
+// createMap function to get the map from json or yaml data
+func createMap(urlsPath []urlPath) map[string]string {
 	pathsToUrls := make(map[string]string, len(urlsPath))
 	for _, pathURL := range urlsPath {
 		pathsToUrls[pathURL.Path] = pathURL.URL
 	}
-	return MapHandler(pathsToUrls, fallback), nil
+	return pathsToUrls
 }
 
+// urlPath structure
 type urlPath struct {
 	Path string `yaml:"path"`
 	URL  string `yaml:"url"`
--- urlshortener/main.go
+++ urlshortener/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 func main() {
+	fileName := flag.String("jsn", "test.json", "the name of the json file consisting short URLs & their corresponsing long URLs")
+	flag.Parse()
 	mux := defaultMux()
 
 	// Build the MapHandler using the mux as the fallback
@@ -28,8 +32,13 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	jsn, err := ioutil.ReadFile(*fileName)
+	if err != nil {
+		panic(err)
+	}
+	jsonHandler, err := JSONHandler([]byte(jsn), yamlHandler)
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	http.ListenAndServe(":8080", jsonHandler)
 }
 
 func defaultMux() *http.ServeMux {
